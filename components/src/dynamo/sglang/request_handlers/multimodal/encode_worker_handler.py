db1c4ba18855967aeb90987ff6b4305bb2f3e7b7
Wang, Yi <yi.a.wang@intel.com>
2026-02-10 11:32:37 +0800

    feat: fix pre-commit error
    Signed-off-by: Wang, Yi <yi.a.wang@intel.com>

--- components/src/dynamo/sglang/request_handlers/multimodal/encode_worker_handler.py
+++ components/src/dynamo/sglang/request_handlers/multimodal/encode_worker_handler.py
@@ -1,8 +1,8 @@
 # SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-import logging
 import asyncio
+import logging
 from contextlib import ExitStack
 from typing import AsyncIterator
 
